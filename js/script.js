c86a22787ab1176b1dd7843eecbcb984339988aa
Raja-Junaid <rajajunaidpdk@gmail.com>
2023-05-08 20:18:58 +0500

    Navbar is fully completed

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 const hamburger = document.querySelector(".hamburger");
 const closeBtn = document.querySelector(".close");
 const navMenu = document.querySelector(".modal");
+const navHero = document.querySelector(".nav-hero");
 
 hamburger.addEventListener("click", function () {
   navMenu.classList.remove("hidden");
   navMenu.classList.add("flex");
-  document.body.classList.add("blur");
+  navHero.classList.add("blureffect");
   document.body.classList.add("no-scroll");
 });
 
 closeBtn.addEventListener("click", function () {
   navMenu.classList.add("hidden");
   navMenu.classList.remove("flex");
+  navHero.classList.remove("blureffect");
   document.body.classList.remove("no-scroll");
 });
 
 navMenu.addEventListener("click", function () {
   navMenu.classList.add("hidden");
   navMenu.classList.remove("flex");
+  navHero.classList.remove("blureffect");
   document.body.classList.remove("no-scroll");
 });
