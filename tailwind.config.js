10b00ca8166fdfbda801726defa4eaae2939ecbd
Raja-Junaid <rajajunaidpdk@gmail.com>
2023-05-08 16:39:59 +0500

    Customize tailwindcss colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      colors: {
+        White: "#ffffff",
+        Black: "#000000",
+        veryDarkGray: "#111111",
+        dimGray: "#6b6b6b",
+        blueViolet: "#664efc",
+        mediumPurple: "#4c42fb",
+      },
+
       gridTemplateColumns: {
         custom: "2fr 1fr",
       },
